653e4d7597e718438462b0e7b118f9865dd37031
April Escobar <aprilfaith.e@gmail.com>
2020-01-22 08:02:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(sentence1)
-  array =  sentence1.split(" ")
+def reverse_each_word(sentence)
+  array =  sentence.split(" ")
   new = array.collect { |word| word.reverse }
   new.join(" ")
 end
